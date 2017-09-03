cc24819227aa5a438046e4c21005e4673f1da992
ROY\roy <cairenjie1985@163.com>
2017-09-03 14:20:20 +0800

    修改配置

--- src/main/java/me/caixin/configuration/MyBatisAutoConfiguration.java
+++ src/main/java/me/caixin/configuration/MyBatisAutoConfiguration.java
@@ -16,6 +16,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.sql.SQLException;
 import java.util.Arrays;
@@ -26,6 +27,7 @@ import java.util.Properties;
  */
 
 @Configuration
+@EnableTransactionManagement
 @MapperScan("me.caixin.dao")
 public class MyBatisAutoConfiguration  implements EnvironmentAware {
 
